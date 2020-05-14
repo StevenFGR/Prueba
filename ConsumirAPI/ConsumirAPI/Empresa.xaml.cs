08f09839de49c25479255ee90620000730bb1c63
unknown <gutierrezrubiosteven@gmail.com>
2020-05-13 22:15:42 -0500

    first commit

---
+++ ConsumirAPI/ConsumirAPI/Empresa.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConsumirAPI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Empresa : ContentPage
+    {
+        public Empresa()
+        {
+            InitializeComponent();
+            LlenarTransportes();
+        }
+
+        private async Task LlenarTransportes()
+        {
+            HttpClient client = new HttpClient();
+
+            
+            HttpResponseMessage request = await client.GetAsync("http://apisteven.somee.com/api/Transportes");
+
+            if (request.IsSuccessStatusCode)
+            {
+                var responseJson = await request.Content.ReadAsStringAsync();
+
+                var response = JsonConvert.DeserializeObject<List<Transportes>>(responseJson);
+
+                listaTransportes.ItemsSource = response;
+            }
+        }
+    }
+}
